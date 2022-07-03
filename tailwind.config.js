40e399d8774c28270b2fc7350b23afcf736d3d4d
Davner <davner23@gmail.com>
2022-07-03 02:01:22 -0400

    q

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
 	theme: {
 		extend: {
 			color: {
-				laranja: '#f44336'
+				color1: '#f46868',
+				color2: '#fca8a0',
+				color3: '#7b0909',
+				color4: '#8e0a0a',
+				color5: '#941104'
 			}
+		},
+		spacing: {
+			'1': '8px',
+			'2': '12px',
+			'3': '16px',
+			'4': '24px',
+			'5': '32px',
+			'6': '48px'
 		}
 	},
 	plugins: []
